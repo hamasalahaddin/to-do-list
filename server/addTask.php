60fb0ba3ec5bdf1ce5041bf7cf0eb03134aad50a
mohammedsalahaddin <mohammedskhoshnaw@gmail.com>
2026-06-29 12:38:51 +0300

    refactor backend configuration

--- server/addTask.php
+++ server/addTask.php
@@ -1,8 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Content-Type: application/json");
-
-require "db.php";
+require "config.php";
 
 $task = $_POST["task"];
 
---
+++ server/config.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Content-Type: application/json");
+
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "todo_db"
+);
+
+mysqli_set_charset($conn, "utf8mb4");
+
+if(!$conn){
+    echo json_encode([
+        "success" => false,
+        "message" => "Database connection failed"
+    ]);
+    exit();
+}
